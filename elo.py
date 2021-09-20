ec00270a3e0657e47e02cc5e8d439fbcca8976d9
Oranen, Leevi <leevi.oranen@gmail.com>
2021-09-20 10:50:59 +0300

    save played games to history file

--- elo.py
+++ elo.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 class Elo:
     def __init__(self,k,g=1,homefield = 100):
@@ -16,23 +17,28 @@ class Elo:
         csv_file.close()
 
     def gameOver(self, winner, loser):
-        """
-        if winnerHome:
-            result = self.expectResult(self.playersDict[winner] + self.homefield, self.playersDict[loser])
-        """
-        #else:
-        result = self.expectResult(self.playersDict[winner], self.playersDict[loser])
+        if winner == loser:
+            print("Check players! Winner = Loser")
+        else:
+            result = self.expectResult(self.playersDict[winner], self.playersDict[loser])
 
-        self.playersDict[winner] = self.playersDict[winner] + (self.k*self.g)*(1 - result)  
-        self.playersDict[loser] 	= self.playersDict[loser] + (self.k*self.g)*(0 - (1 -result))
-        self.save_elos()
+            self.playersDict[winner] = self.playersDict[winner] + (self.k*self.g)*(1 - result)  
+            self.playersDict[loser] 	= self.playersDict[loser] + (self.k*self.g)*(0 - (1 -result))
+            self.save_elos(winner, loser)
 
-    def save_elos(self):
+    def save_elos(self, winner, loser):
         with open('saved_elos.csv', 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(["player", "elo"])
             for player in self.playersDict:
                 writer.writerow([player, self.playersDict[player]])
+        csv_file.close()
+
+        now = datetime.now()
+        timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
+        with open('history.csv', 'a+', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([timestamp, winner, round(self.playersDict[winner]), loser, round(self.playersDict[loser], 2)])
         csv_file.close()
 
     def expectResult(self, p1, p2):
@@ -59,6 +65,22 @@ class Elo:
             with open('saved_elos.csv', 'w', newline='') as csv_file:
                 writer = csv.writer(csv_file)
                 writer.writerow(["player", "elo"])
+            csv_file.close()
+
+        try:
+            with open('history.csv') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line_count = 0
+                for row in csv_reader:
+                    print(row)
+
+                csv_file.close()
+
+        except FileNotFoundError:
+            print("File not found!")
+            with open('history.csv', 'w', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(["time", "winner", "winnerNewElo", "loser", "loserNewElo"])
             csv_file.close()
 
 
